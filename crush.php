30d57b56849cc2e75c68052dc8fe76477ef8b90c
yugesh1427 <43095896+yugesh1427@users.noreply.github.com>
2022-08-17 12:16:59 +0100

    Update crush.php

--- crush.php
+++ crush.php
@@ -60,7 +60,8 @@
 	<h4>My Biggest Wish</h4>
 	<h6>Enter the below Field To See</h6>
 	<p> <input id="text1" placeholder="Teddy's Name"  autocomplete="off" type="text"></p>
-	<p><button class="buttonk" id="trigger"  onclick="myFunction();">Enter</button></p>
+
+	<p><button type="submit" value="Submit" class="buttonk" id="trigger"  onclick="myFunction();">Enter</button></p>
   </form>
   
   <div class="x" id="myDIV">
