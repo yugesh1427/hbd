b6e9c94284762201745310b033f855e9a1dccce4
yugesh1427 <43095896+yugesh1427@users.noreply.github.com>
2022-08-19 14:23:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ if (((bday == 366) && (leapyear(gyear)) || ((bday == 365) && (!leapyear(gyear)))
 document.agecalc.timealive.value += "\n\n             Happy happy birthday! \n\n        Hope our bonding will continue forever.";
 } else {
 document.agecalc.timealive.value += "\n\n           And, next Beautiful day is in:\n        "
-+ bday-1 + " days " + nhour + " hrs " + nmin + " mins " + nsec + " secs \n\n         Hope our bonding will continue forever.";
++ bday-1 + " days " + nhour-1 + " hrs " + nmin + " mins " + nsec + " secs \n\n         Hope our bonding will continue forever.";
 setTimeout("run()", 1000);
    }
 }
