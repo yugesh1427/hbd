2b97ced93f0f7f0a323a76b99b70e253d5198f3a
yugesh1427 <43095896+yugesh1427@users.noreply.github.com>
2022-08-19 11:42:45 +0100

    Update index.php

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
 <div class="box" id="y">
 	<h1>Hello</h1>
 	<h2>Mam 😈</h2>
-	<p> <input id="form1" placeholder="Baby Girls's Name👸"  autocomplete="off" required="" type="text"></p>
-  <p> <input id="form2" placeholder="Moon's Name🌝"  autocomplete="off" required="fill" type="text"></p>
+	<p> <input id="form1" placeholder="Baby Girls's Name👸"  autocomplete="off" required type="text"></p>
+  <p> <input id="form2" placeholder="Moon's Name🌝"  autocomplete="off" required type="text"></p>
   <p><button class="buttonk" id="trigger" onclick="fun();">Enter</button></p>
   </div></div>
   <style>
